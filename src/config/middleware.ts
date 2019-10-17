7d339a7cb7f525101b28e3b6797a24ddd617c0e9
richen <richenlin@gmail.com>
2019-10-17 11:59:12 +0800

    Use typescript

--- src/config/middleware.ts
+++ src/config/middleware.ts
@@ -2,13 +2,24 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2019-10-12 14:11:06
+ * @ version: 2019-10-17 11:55:50
  */
 export default {
-    list: [], //加载的中间件列表
+    list: ['TypeormMid', 'TestMid'], //加载的中间件列表
     config: { //中间件配置
-        // static: {
-        //     cache: false
-        // },
+        TestMid: {
+            test: 1
+        },
+        TypeormMid: {
+            "type": "mysql", //mysql, mariadb, postgres, sqlite, mssql, oracle, mongodb, cordova
+            "host": "192.168.0.150",
+            "port": 3306,
+            "username": "test",
+            "password": "test",
+            "database": "test",
+            "synchronize": false, //true 每次运行应用程序时实体都将与数据库同步
+            "logging": true,
+            "entities": [`${process.env.APP_PATH}/model/*`]
+        }
     }
 };
--- src/service/TestService.ts
+++ src/service/TestService.ts
@@ -2,19 +2,37 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2019-10-17 11:41:29
+ * @ version: 2019-10-17 11:57:11
  */
 import { App } from "../App";
-import { Service, Autowired } from "koatty";
+import { Service, Autowired, logger } from "koatty";
 import { UserModel } from "../model/UserModel";
+import { TestModel } from "../model/TestModel";
+import { Connection } from "typeorm";
 
 @Service()
 export class TestService {
     public app: App;
+    private connection: Connection;
     @Autowired('UserModel', 'COMPONENT')
     private userModel: UserModel;
 
+    init() {
+        this.connection = this.app.connection;
+    }
+
+
     public sayHello() {
         return this.userModel.find();
+    }
+
+    async sayHello2(param: string) {
+        logger.info('TestService.sayHello2', param);
+        return TestModel.findOne();
+    }
+
+    async typeSave() {
+        const testRepository = this.connection.getRepository(TestModel);
+        return testRepository.findOne();
     }
 }
