95c0b9ee29165a753646cae343c7f177b265c0d6
linyyyang <linyyyang@tencent.com>
2020-07-28 11:36:48 +0800

    update

--- src/controller/Web/WebIndexController.ts
+++ src/controller/Web/WebIndexController.ts
@@ -4,8 +4,9 @@
  * @ license: Apache License 2.0
  * @ version: 2020-05-10 15:02:26
  */
-import { Controller, BaseController, GetMapping } from "koatty";
+import { Controller, BaseController, GetMapping, RequestMapping, RequestMethod } from "koatty";
 import { App } from '../../App';
+const request = require("think_request");
 
 @Controller()
 export class WebIndexController extends BaseController {
@@ -18,5 +19,11 @@ export class WebIndexController extends BaseController {
     @GetMapping("/")
     index() {
         return this.ctx.render('./html/index.html');
+    }
+
+    @RequestMapping("/client", RequestMethod.GET)
+    async httpClient() {
+        const res = await request({}, "https://baidu.com");
+        return res;
     }
 }
