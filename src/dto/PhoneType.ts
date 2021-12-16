2dd75e1ad1fe4a6cd2deb4819496ad44168dc4b3
linyyyang <linyyyang@tencent.com>
2021-12-16 11:39:41 +0800

    🐞 fix:

--- src/dto/PhoneType.ts
+++ src/dto/PhoneType.ts
@@ -3,12 +3,12 @@
  * @Usage: 
  * @Author: xxx
  * @Date: 2020-12-22 15:24:25
- * @LastEditTime: 2021-11-25 12:50:45
+ * @LastEditTime: 2021-12-16 09:57:36
  */
 
 export enum PhoneType {
 
-  MOBILE = 0,
-  HOME = 1,
-  WORK = 2,
+  MOBILE = "0",
+  HOME = "1",
+  WORK = "2",
 }
