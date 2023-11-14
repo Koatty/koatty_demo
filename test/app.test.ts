f2b1bd1984ff625e914e0d31fffd1475f8543134
linyyyang <linyyyang@tencent.com>
2023-11-14 20:12:04 +0800

    fix: deps

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,7 +3,7 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2022-03-10 11:49:05
- * @LastEditTime: 2022-03-10 19:01:59
+ * @LastEditTime: 2023-11-14 10:18:04
  */
 import request from 'supertest';
 import { ExecBootStrap } from 'koatty';
